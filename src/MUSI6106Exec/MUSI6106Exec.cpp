13991e8d6ecf60f82f8989297601d17c7e100728
Yanchao Liu <2452926972@qq.com>
2020-01-22 21:56:21 -0500

    Update MUSI6106Exec.cpp

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -5,12 +5,51 @@
 #include "MUSI6106Config.h"
 
 #include "AudioFileIf.h"
+#include "CombFilterIf.h"
 
 using std::cout;
 using std::endl;
 
 // local function declarations
 void    showClInfo ();
+void    iirFilter(float** prtInput, float** prtOutput, int iFileLength, int iChannels, float fs, float t0, float g, float c)
+{
+    int iNumOfDelay = int(t0 * fs);
+    for(int i = 0; i<iChannels; i++)
+    {
+        for(int j = 0; j<iFileLength; j++)
+        {
+            if(j>=iNumOfDelay)
+            {
+                prtOutput[i][j] = c*prtInput[i][j] + g*prtOutput[i][j - iNumOfDelay];
+            }
+            else
+            {
+                prtOutput[i][j] = c*prtInput[i][j];
+            }
+        }
+    }
+}
+
+bool firFilter(float** prtInput, float** prtOutput, int iFileLength, int iChannels, float fs, float t0, float g)
+{
+    int iNumOfDelay = int(t0 * fs);
+    for(int i = 0; i<iChannels; i++)
+    {
+        for(int j = 0; j<iFileLength; j++)
+        {
+            if(j>=iNumOfDelay)
+            {
+                prtOutput[i][j] = prtInput[i][j] + g*prtInput[i][j - iNumOfDelay];
+            }
+            else
+            {
+                prtOutput[i][j] = prtInput[i][j];
+            }
+        }
+    }
+    return true;
+}
 
 /////////////////////////////////////////////////////////////////////////////////
 // main function
@@ -28,80 +67,212 @@ int main(int argc, char* argv[])
     CAudioFileIf            *phAudioFile = 0;
     std::fstream            hOutputFile;
     CAudioFileIf::FileSpec_t stFileSpec;
-    CAudioFileIf::FileSpec_t* ptr_stFileSpec={&stFileSpec};
+    CAudioFileIf::FileSpec_t *pStFileSpec = &stFileSpec;
     
-    
-//    My variables
+    float filterDelayTime = {0.0002268};
+    std::string filterType;
+    float filterGain = {0.5};
+    float filterIirC = {1};
+    int iNumSampleDelay = {0};
+    long long iLengthInFrame = {0};
+    float **ppfOutputData = 0;
+    float **ppfInputData = 0;
+    CAudioFileIf *phOutputAudioFile = 0;
 
     showClInfo();
-
+//  inputFilePath, outputFilePath, filterDelayTime, filterType, filterGain,
     //////////////////////////////////////////////////////////////////////////////
     // parse command line arguments
-//    sInputFilePath = argv[2];
-    sInputFilePath = "/Users/yanchaoliu/Study/Spring 2020 Local/MUSI 6106 Audio Software Eng/Demo_Project/Audio/sweep.wav";
-    sOutputFilePath = "/Users/yanchaoliu/Study/Spring 2020 Local/MUSI 6106 Audio Software Eng/Demo_Project/Text/output.txt";
-    std::cout << sInputFilePath << std::endl;
-    //////////////////////////////////////////////////////////////////////////////
-    // open the input wave file
-    phAudioFile->create (phAudioFile);
-    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
-    phAudioFile->getFileSpec(stFileSpec);
-    //////////////////////////////////////////////////////////////////////////////
-    // open the output text file
-    hOutputFile.open(sOutputFilePath.c_str(), std::ios::out);
-    if(!hOutputFile.is_open())
-    {
-        std::cout << "Open text file error" << std::endl;
-    }
+//    if (argc == 1)
+//    {
+//        cout << "Running default test cases" << endl;
+//        return -1;
+//    }
+//    if (argc != 6)
+//    {
+//        cout << "Please parse argument by 'inputFilePath, outputFilePath, filterDelayTime, filterType, filterGain'" << endl;
+//        return -1;
+//    }
+
+    sInputFilePath = argv[1];
+    sOutputFilePath = argv[2];
+    filterDelayTime = atof(argv[3]);
+    filterType = argv[4];
+    filterGain = atof(argv[5]);
+    filterIirC = atof(argv[6]);
+    
+    
+    sInputFilePath = "/Users/yanchaoliu/Study/Spring\ 2020\ Local/MUSI\ 6106\ Audio\ Software\ Eng/Demo_Project/Audio/sweep.wav";
+    sOutputFilePath = "/Users/yanchaoliu/Study/Spring\ 2020\ Local/MUSI\ 6106\ Audio\ Software\ Eng/Demo_Project/Audio/sweep_output.wav";
+
+
     
     //////////////////////////////////////////////////////////////////////////////
+    // open the input wave file
+    CAudioFileIf::create(phAudioFile);
+    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    if (!phAudioFile->isOpen())
+    {
+        cout << "Wave file open error!";
+        return -1;
+    }
+    
+    phAudioFile->getFileSpec(stFileSpec);
+    phAudioFile->getLength(iLengthInFrame);
+    cout << iLengthInFrame << endl;
+
+    //////////////////////////////////////////////////////////////////////////////
+    // open the output wav file
+    CAudioFileIf::create(phOutputAudioFile);
+    phOutputAudioFile->openFile(sOutputFilePath, CAudioFileIf::kFileWrite, pStFileSpec);
+    if (!phOutputAudioFile->isOpen())
+    {
+        cout << "Output file open error!";
+        return -1;
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-    long long int length = 0;
-
-    phAudioFile->getLength(length);
-    int iChannel = stFileSpec.iNumChannels;
-//    int iNumOfSample = (*phAudioFile).getLength();
-    ppfAudioData = new float* [iChannel];
-    long long iNumSample = kBlockSize;
-    for(int i=0;i<iChannel;i++)
+    ppfAudioData = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
         ppfAudioData[i] = new float[kBlockSize];
-    //////////////////////////////////////////////////////////////////////////////
-    // get audio data and write it to the output text file (one column per channel)
-    while(!phAudioFile->isEof())
-    {
-        phAudioFile->readData(ppfAudioData, iNumSample);
-        for(int j = 0; j<iNumSample; j++)
-        {
-            for(int i = 0; i<iChannel; i++)
-            {
-                hOutputFile << ppfAudioData[i][j] << "\t";
-            }
-            hOutputFile << std::endl;
-        }
-    }
+    
+    ppfOutputData = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfOutputData[i] = new float[iLengthInFrame];
+    
+    ppfInputData = new float*[stFileSpec.iNumChannels];
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        ppfInputData[i] = new float[iLengthInFrame];
 
+    time = clock();
     //////////////////////////////////////////////////////////////////////////////
-    // clean-up (close files and free memory)
-    phAudioFile->closeFile();
-    phAudioFile->destroy(phAudioFile);
-    hOutputFile.close();
-    for(int i = 0; i<iChannel; i++)
+    // get audio data and write it to the output file
+    int iPosition = 0;
+    while (!phAudioFile->isEof())
     {
-        delete[] ppfAudioData[i];
+        long long iNumFrames = kBlockSize;
+        phAudioFile->readData(ppfAudioData, iNumFrames);
+        
+        cout << "\r" << "reading and writing";
+        phOutputAudioFile->writeData(ppfAudioData, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++)
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                ppfOutputData[c][i + iPosition] = ppfAudioData[c][i];
+                ppfInputData[c][i + iPosition] = ppfAudioData[c][i];
+            }
+        }
+        iPosition += iNumFrames;
     }
-    delete [] ppfAudioData;
-    // all done
+    //cout << (ppfOutputData[0][0]) << endl;
+    //phOutputAudioFile->writeData(ppfOutputData, iLengthInFrame);
+    
+
+    cout << "\nreading/writing done in: \t" << (clock() - time)*1.F / CLOCKS_PER_SEC << " seconds." << endl;
+    //////////////////////////////////////////////////////////////////////////////
+    // process audio data
+//    firFilter(ppfInputData, ppfOutputData, iLengthInFrame, stFileSpec.iNumChannels, stFileSpec.fSampleRateInHz, filterDelayTime, filterGain);
+    firFilter(ppfInputData, ppfOutputData, iLengthInFrame, stFileSpec.iNumChannels, stFileSpec.fSampleRateInHz, filterDelayTime, filterGain);
+//    iirFilter(ppfInputData, ppfOutputData, iLengthInFrame, stFileSpec.iNumChannels, stFileSpec.fSampleRateInHz, filterDelayTime, filterGain, filterIirC);
+    //////////////////////////////////////////////////////////////////////////////
+    // output audio data
+    iPosition = 0;
+    while (!phAudioFile->isEof())
+    {
+        long long iNumFrames = kBlockSize;
+        phAudioFile->readData(ppfAudioData, iNumFrames);
+        
+        cout << "\r" << "reading and writing";
+        phOutputAudioFile->writeData(ppfAudioData, iNumFrames);
+        for (int c = 0; c < stFileSpec.iNumChannels; c++)
+        {
+            for (int i = 0; i < iNumFrames; i++)
+            {
+                ppfAudioData[c][i] = ppfAudioData[c][i+iPosition];
+            }
+        }
+        iPosition += iNumFrames;
+        phOutputAudioFile->writeData(ppfAudioData, iNumFrames);
+    }
+    
+    
+    //////////////////////////////////////////////////////////////////////////////
+    // clean-up
+    CAudioFileIf::destroy(phAudioFile);
+    CAudioFileIf::destroy(phOutputAudioFile);
+
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfAudioData[i];
+    delete[] ppfAudioData;
+    ppfAudioData = 0;
+    
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfInputData[i];
+    delete[] ppfInputData;
+    ppfInputData = 0;
+    
+    for (int i = 0; i < stFileSpec.iNumChannels; i++)
+        delete[] ppfOutputData[i];
+    delete[] ppfOutputData;
+    ppfOutputData = 0;
+
     return 0;
 
 }
 
 
+
+// input: pointer of buffer
+// output: none
+// modify the input buffer with FIR or IIR filter
+//void firFilter(const float** prtInput, float** prtOutput, const int iFileLength, const int iChannels, const float fs, const float t0, const float g)
+//{
+//    int iNumOfDelay = int(t0 * fs);
+//    for(int i = 0; i<iChannels; i++)
+//    {
+//        for(int j = 0; j<iFileLength; j++)
+//        {
+//            if(j>=iNumOfDelay)
+//            {
+//                prtOutput[i][j] = prtInput[i][j] + g*prtInput[i][j - iNumOfDelay];
+//            }
+//            else
+//            {
+//                prtOutput[i][j] = prtInput[i][j];
+//            }
+//        }
+//    }
+//
+//}
+
+//void IirFilter(const float** prtInput, float** prtOutput, const int iFileLength, const int iChannels, const float fs, const float t0, const float g, const float c)
+//{
+//    int iNumOfDelay = int(t0 * fs);
+//    for(int i = 0; i<iChannels; i++)
+//    {
+//        for(int j = 0; j<iFileLength; j++)
+//        {
+//            if(j>=iNumOfDelay)
+//            {
+//                prtOutput[i][j] = c*prtInput[i][j] + g*prtOutput[i][j - iNumOfDelay];
+//            }
+//            else
+//            {
+//                prtOutput[i][j] = c*prtInput[i][j];
+//            }
+//        }
+//    }
+//}
+
 void     showClInfo()
 {
     cout << "GTCMT MUSI6106 Executable" << endl;
-    cout << "(c) 2014-2020 by Alexander Lerch" << endl;
+    cout << "(c) 2014-2020 by Alexander Lerch!!" << endl;
     cout  << endl;
 
     return;
 }
+
 
